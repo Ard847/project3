b2ce26ba04b69fdf7375368955e6e7bec6b6db12
Improbdrive <sam.wakelam@hotmail.com>
2021-03-11 12:28:25 +0000

    get dashboard and welcome pages rendering seperately

---
+++ client/src/components/WelcomeTitles.js
@@ -0,0 +1,17 @@
+// packages
+import React from 'react';
+
+// styles
+import './WelcomeTitles.css';
+
+const WelcomeTitles = () => {
+  return (
+    <section id='app-titles'>
+      <h1 id='main-title'>Welcome to Household Manager.</h1>
+      <h3>Manage your Home with style.</h3>
+
+    </section>
+  );
+}
+
+export default WelcomeTitles;
---
+++ client/src/pages/welcomePages/HomePage.js
@@ -0,0 +1,92 @@
+// packages
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+
+// styles
+import './HomePage.css';
+
+// context
+import LoggedInContext from "../../context/LoggedInContext";
+
+// hooks
+import useGetHouseholds from '../../hooks/useGetHouseholds';
+import useSiteLocation from '../../hooks/useSiteLocation';
+
+// components
+import WelcomeTitles from '../../components/WelcomeTitles';
+
+
+
+const HomePage = ({ location }) => {
+
+  const { loggedIn } = useContext(LoggedInContext);
+  useSiteLocation(location);
+
+  const households = useGetHouseholds();
+  console.log('households =', households);
+
+  if (loggedIn === false) {
+
+    return (
+      <>
+        <WelcomeTitles />
+        <section>
+          <article id='home-content'>
+            <h2>Log In or Sign Up to see your Households</h2>
+          </article>
+        </section>
+      </>
+
+    )
+
+  } else if (loggedIn === true) {
+
+    return (
+      <>
+        <WelcomeTitles />
+        <section>
+          <article id='home-content' className='flex'>
+
+            <div id='create-household'>
+              <NavLink
+                to='/createHousehold'
+              >
+                <img
+                  className='household-img'
+                  src=''
+                  alt=''
+                />
+                <p>Create New Household</p>
+              </NavLink>
+            </div>
+
+            <div id='current-household' className='flex'>
+
+              {households.map((household) => {
+                return (
+                  <div key={household.id} className='household'>
+                    <NavLink to={`/dashboard/${household.id}`} >
+                      <img
+                        className='household-img'
+                        src=''
+                        alt=''
+                      />
+                      <p>{household.houseName}</p>
+                    </NavLink>
+                  </div>
+                )
+              })}
+
+            </div>
+
+          </article>
+        </section>
+      </>
+
+    )
+
+  }
+
+}
+
+export default HomePage;
