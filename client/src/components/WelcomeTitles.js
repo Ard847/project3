757387a9a76dbfa17e0ef408080d243e79537302
Sumaya Hassan <sumaya_198@hotmail.co.uk>
2021-03-17 22:33:54 +0000

    added background image

--- client/src/components/WelcomeTitles.js
+++ client/src/components/WelcomeTitles.js
@@ -1,17 +1,31 @@
 // packages
 import React from 'react';
 
+//image
+
+
 // styles
 import './WelcomeTitles.css';
 
 const WelcomeTitles = () => {
   return (
-    <section id='app-titles'>
-      <h1 id='main-title'>Welcome to Household Manager.</h1>
-      <h3>Manage your Home with style.</h3>
+    <div>
+   <header id="home">
+<div className="container-fluid header-content">
+  <div className="row">
+    <div className="col-lg-6 text-start">
+      <div className="content-box">
+        <h4 className="title">Manage Your Home</h4>
+        
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, porro!</p>
+      </div>
+    </div>
+  </div>
+</div>
+</header>
 
-    </section>
-  );
+    </div>
+)
 }
 
 export default WelcomeTitles;
