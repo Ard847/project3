8d17b16adb032b6660732508bbbbd76c233961f1
Sumaya Hassan <sumaya_198@hotmail.co.uk>
2021-03-17 14:42:18 +0000

    Navbar

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 // context
 import { LocationContextProvider } from './context/LocationContext';
 import { LoggedInContextProvider } from './context/LoggedInContext';
