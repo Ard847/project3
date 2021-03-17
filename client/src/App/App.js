38382c5ab56c01c02c72ecbc4b89363bbe279713
Sumaya Hassan <sumaya_198@hotmail.co.uk>
2021-03-17 21:54:27 +0000

    added nav bar

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -18,7 +18,7 @@ import CreateHouse from '../pages/welcomePages/CreateHouse';
 import ChooseHouse from '../pages/welcomePages/ChooseHouse';
 // import NoMatchPage from '../pages/welcomePages/NoMatchPage';
 
-import DashApp from '../pages/dashboardPages/DashApp';
+//import DashApp from '../pages/dashboardPages/DashApp';
 
 // components
 import NavBar from '../components/Navbar';
@@ -42,7 +42,7 @@ const App = () => {
             <Route exact path='/logIn' component={LoginPage} />
             <Route exact path='/signUp' component={SignupPage} />
             <Route exact path='/createHousehold' component={CreateHouse} />
-            <Route path='/dashboard/:id/:householdID' component={DashApp} />
+           
             {/* <Route component={NoMatchPage} /> */}
           </main>
         {/* </Switch> */}
@@ -52,3 +52,5 @@ const App = () => {
 }
 
 export default App;
+
+/* <Route path='/dashboard/:id/:householdID' component={DashApp} /> */
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,49 @@
 // packages
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-
+import React, { useState } from 'react';
+import {Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,} from 'reactstrap';
+  
 // styles
 import './NavBar.css'
 
 
-const Navbar = () => {
-  return (
-    <nav id='main-nav'>
-      <p id='brand'>Logo</p>
-      <NavLink to='/'>Home</NavLink>
-      <NavLink to='/about'>About</NavLink>
-      <NavLink to='/logIn'>Log In </NavLink>
-      <NavLink to='/signUp'>Sign Up</NavLink>
-    </nav>
-  )
-}
+const NavBar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Navbar;
+  const toggle = () => setIsOpen(!isOpen);
+    return (
+      <div>
+        <Navbar className="sticky fixed-top" light expand="md">
+          <NavbarBrand href="/"><img /></NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto items" navbar>
+              <NavItem>
+                <NavLink style={{fontSize: '15px', letterSpacing: '0.05em', marginLeft: '20px'}} href="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={{fontSize: '15px', letterSpacing: '0.05em', marginLeft: '20px'}} href="/about">About</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={{fontSize: '15px', color: 'white', letterSpacing: '0.05em', marginLeft: '20px'}} href="/contact">Contact</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={{fontSize: '15px', color: 'white', letterSpacing: '0.05em', marginLeft: '20px'}} href="/logIn">Login</NavLink>
+              </NavItem>
+            </Nav>
+            <NavItem>
+                <NavLink style={{fontSize: '15px', color: 'black', letterSpacing: '0.05em', marginLeft: '20px'}} href="/signUp">Sign up</NavLink>
+              </NavItem>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+  
+
+export default NavBar;
