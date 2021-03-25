db6a43f471788e52c0db224cf12875a7ae207ab3
ard847 <renatopombal7@gmail.com>
2021-03-25 21:18:14 +0000

    heroku

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static(path.join(__dirname, "../client/build")))
 }
 
 app.use(express.urlencoded({limit:'50mb', extended: true }));
