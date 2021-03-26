9ea13517cbfc73c8d9063ffbe89e926766cae907
Improbdrive <sam.wakelam@hotmail.com>
2021-03-26 19:42:53 +0000

    fixing loading issue

--- client/src/context/MembersContext.js
+++ client/src/context/MembersContext.js
@@ -1,28 +1,34 @@
 // packages
-import { useState, createContext, useEffect } from 'react';
+import { useState, createContext, useEffect, useContext, useCallback } from 'react';
 
 // functions
 import getSession from '../functions/getSession';
 import fetcher from '../functions/fetcher';
+// context
+import LoggedInContext from './LoggedInContext';
 
 // create context 
 const MembersContext = createContext();
 
 
-const MembersContextProvider = ({children}) => {
+const MembersContextProvider = ({ children }) => {
 
-  const [ members, setMembers ] = useState([]);
+  const [members, setMembers] = useState([]);
+  const { loggedIn } = useContext(LoggedInContext);
 
-  const fetchData = async () => {
-    const houseID = getSession('houseID');
-    let token = getSession('token').split('"');
-    token = token[1];
+  const fetchData = useCallback(async () => {
+    if (loggedIn) {
+      const houseID = getSession('houseID');
+      let token = getSession('token').split('"');
+      token = token[1];
 
-    const url = `/api/user/getusers/${houseID}`;
-    const userResponse = await fetcher( url, 'GET','',token); 
-    // console.log(`userResponse called =`, userResponse);
-    setMembers(userResponse);
-  };
+      const url = `/api/user/getusers/${houseID}`;
+      const userResponse = await fetcher(url, 'GET', '', token);
+      // console.log(`userResponse called =`, userResponse);
+      setMembers(userResponse);
+    }
+
+  }, [loggedIn]);
 
   const refreshMembers = () => {
     // console.log('refreshing');
@@ -34,7 +40,7 @@ const MembersContextProvider = ({children}) => {
     fetchData();
     return () => {
       console.log(`I did unmount`);
-    }; 
+    };
 
   }, []);
 
