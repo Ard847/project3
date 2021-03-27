cd54635dfa84a7dc0be5110b5dfc54fd2542d639
Improbdrive <sam.wakelam@hotmail.com>
2021-03-27 14:15:01 +0000

    deployment

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,8 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-console.log("proce ",process.env.DB_NAME)
-const connection = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
+let connection;
+if (process.env.JAWSDB_URL){
+  connection = new Sequelize(process.env.JAWSDB_URL, {
     dialect: 'mysql',
     pool: {
       max: 5,
@@ -14,8 +10,25 @@ const connection = new Sequelize(
       acquire: 30000,
       idle: 10000,
     },
-  }
-);
+  });
+} else {
+  connection = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+      },
+    }
+  );
+
+}
 
 
 module.exports = connection;
